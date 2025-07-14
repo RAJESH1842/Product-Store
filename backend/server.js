1cee1ed09af0df37c1ff3a462faf962012caa44c
Rajesh <rajeshsrrs2@gmail.com>
2025-07-14 23:44:32 +0530

    added some data in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello from the backend");
-})
+console.log(PORT);
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.get("/api/products", (req, res) => {
+    
+    res.status(200).json({
+        success: true,
+        data: [
+            { id: 1, name: "product 1"},
+            { id: 2, name: "product 2"},
+            { id: 3, name: "product 3"},
+        ],
+    });
 });
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+});
+  
+
+    
