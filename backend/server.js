3b584577472f29b48e3e78d9e6fb47e5a9c324c4
Rajesh <rajeshsrrs2@gmail.com>
2025-07-17 15:34:20 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import { sql } from "../backend/config/db.js"; 
+
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-console.log(PORT);
 
 app.use(express.json());
 app.use(cors());
@@ -17,14 +19,29 @@ app.use(helmet());
 
 app.use(morgan("dev"));
 
-app.get("/api/products", (req, res) => {
-    
-   
-});
+app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
+async function initDB() {
+    try {
+        await sql `
+        CREATE TABLE IF NOT EXISTS products(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(225) NOT NULL,
+        image VARCHAR(225) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        `;
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.log("Error initDB" , error);
+    }
+}
+
+initDB().then( () => {
+    app.listen(PORT, ()=>{
+        console.log("Server is running on port " + PORT);
+    });
 });
-  
 
     
