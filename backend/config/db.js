61307e8b02b7090c0d015290ed43448a0f0739ff
Rajesh <rajeshsrrs2@gmail.com>
2025-08-31 20:23:34 +0530

    API testing

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,11 @@ dotenv.config();
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
 
+// creates a sql connection using our env variables 
 export const sql = neon(
     `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`
 )
+
+//this sql function we export is used to query as a tagged template literal, which allows us to write SQL queries safely
 
 // postgresql://neondb_owner:npg_W5TFeKHfAy7R@ep-wild-paper-a8q8pwqa-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require
