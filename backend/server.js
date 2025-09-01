725f86475103b7adb5284da2fa9fd3d83da08da5
Rajesh <rajeshsrrs2@gmail.com>
2025-09-01 21:32:34 +0530

    arcjet rate limitation

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
@@ -17,7 +17,38 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-app.use(morgan("dev"));
+app.use(morgan("dev")); 
+
+//apply arcjet rate-limit to all routes
+
+app.use(async (req, res, next) => {
+    try{
+        const decision = await aj.protect(req, {
+            requested: 1,
+        });
+        if(decision.isDenied()) {
+            if (decision.reason.isRateLimit()){
+                res.status(429).json({error: "Too Many Requests"});
+            } else if (decision.reason.isBot()){
+                res.status(403).json({error: "Bot access denied"});
+            }else {
+                res.status(403).json({error: "Forbidden"});
+            }
+            return
+        }
+
+        if(decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())){
+            res.status(403).json({ error: "Spoofed bot detected"});
+            return;
+        }
+
+        next()
+    } catch (error) {
+        console.log("Arcjet error", error);
+        next(error);
+    }
+});
+
 
 app.use("/api/products", productRoutes);
 
